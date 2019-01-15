c327e98aa103f8aa1b489dd5ea9eae689560fdc0
Fen Lombardi and Ryan McLean <fen+ryan@>
2019-01-14 16:30:30 -0800

    Add conversions gallons to ounces, cups and tablespoons, and cups to tablespoons.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//Gallons to ounces
+var oz = parseInt(prompt("How many gallons?"));
+var oz = oz * 128;
+alert(oz + " Ounces.");
+
+//Gallons to cups
+var cups = parseInt(prompt("How many gallons?"));
+var cups = cups * 16;
+alert(cups + " Cups.");
+
+//Cups to tablespoons
+var tbsp = parseInt(prompt("How many cups?"));
+var tbsp = tbsp * 16;
+alert(tbsp + " tablespoons.");
+
+//Gallons to tablespoons
+var tbsp = parseInt(prompt("How many gallons?"));
+var tbsp = tbsp * 256;
+alert(tbsp + " tablespoons.");
